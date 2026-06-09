d312272b0ab9fb3d579fa5d92dad1a0ac3cbd99f
Meys Torkaman <meys.torkaman@bjss.com>
2026-06-09 10:39:11 +0100

    updated column type

--- sql/00015_create_vehicle_load_status_table.sql
+++ sql/00015_create_vehicle_load_status_table.sql
@@ -1,16 +1,32 @@
 --liquibase formatted sql
 --changeset liquibase:addColumn -multiple-tables:1 splitStatements:true endDelimiter:; context:dev
 
-CREATE TABLE IF NOT EXISTS `vehicle_load_status`
-(
-    `id`                           BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `test_type_id`                 BIGINT UNSIGNED NOT NULL,
-    `load_status`                  VARCHAR(30) NULL,
-    `unladen_body_type`            VARCHAR(27) NULL,
-    `other_unladen_body_type`      VARCHAR(200) NULL,
-    `reason_for_not_loading`       VARCHAR(20) NULL,
-    `other_reason_for_not_loading` VARCHAR(200) NULL,
-    `partially_laden_reason`       VARCHAR(200) NULL,
+CREATE TABLE IF NOT EXISTS `load_status` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `load_status` VARCHAR(30) NULL,
     PRIMARY KEY (`id`)
-)
-    ENGINE = InnoDB;
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `unladen_body_type` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `unladen_body_type` VARCHAR(27) NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `reason_for_not_loading` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `reason_for_not_loading` VARCHAR(20) NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `vehicle_load_status` (
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `test_type_id` BIGINT UNSIGNED NOT NULL,
+    `load_status_id`BIGINT UNSIGNED NULL,
+    `unladen_body_type_id` BIGINT UNSIGNED NULL,
+    `other_unladen_body_type` VARCHAR(200) NULL,
+    `reason_for_not_loading_id`BIGINT UNSIGNED NULL,
+    `other_reason_for_not_loading` VARCHAR(200) NULL,
+    `partially_laden_reason` VARCHAR(200) NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
