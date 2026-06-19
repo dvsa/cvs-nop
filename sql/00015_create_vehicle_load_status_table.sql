c22ebdc7db1dd94c5033e25e7ec8bbe5fe38adb6
Meys Torkaman <meys.torkaman@bjss.com>
2026-06-19 17:43:26 +0100

    added UNIQUE INDEX to vehicle load status tables

--- sql/00015_create_vehicle_load_status_table.sql
+++ sql/00015_create_vehicle_load_status_table.sql
@@ -4,31 +4,34 @@
 CREATE TABLE IF NOT EXISTS `load_status` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `load_status` VARCHAR(30) NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `idx_load_status_uq` (`load_status`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `unladen_body_type` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `unladen_body_type` VARCHAR(27) NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `idx_unladen_body_type_uq` (`unladen_body_type`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `reason_for_not_loading` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `reason_for_not_loading` VARCHAR(20) NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `idx_reason_for_not_loading_uq` (`reason_for_not_loading`)
 ) ENGINE = InnoDB;
 
 CREATE TABLE IF NOT EXISTS `vehicle_load_status` (
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     `test_type_id` BIGINT UNSIGNED NOT NULL,
-    `load_status_id`BIGINT UNSIGNED NULL,
+    `load_status_id` BIGINT UNSIGNED NULL,
     `unladen_body_type_id` BIGINT UNSIGNED NULL,
     `other_unladen_body_type` VARCHAR(200) NULL,
-    `reason_for_not_loading_id`BIGINT UNSIGNED NULL,
+    `reason_for_not_loading_id` BIGINT UNSIGNED NULL,
     `other_reason_for_not_loading` VARCHAR(200) NULL,
     `partially_laden_reason` VARCHAR(200) NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
     FOREIGN KEY (`load_status_id`)
         REFERENCES load_status (`id`)
         ON DELETE NO ACTION
@@ -42,6 +45,8 @@ CREATE TABLE IF NOT EXISTS `vehicle_load_status` (
     FOREIGN KEY (`reason_for_not_loading_id`)
         REFERENCES reason_for_not_loading (`id`)
         ON DELETE NO ACTION
-        ON UPDATE NO ACTION
+        ON UPDATE NO ACTION,
+    
+    UNIQUE INDEX `idx_vehicle_load_status_uq` (`test_type_id`, `load_status_id`, `unladen_body_type_id`, `other_unladen_body_type`, `reason_for_not_loading_id`, `other_reason_for_not_loading`, `partially_laden_reason`)
 
 ) ENGINE = InnoDB;
